ac90db5c8efc466c7194a51d084b3cca8fa41635
HOAI NAM <hoainam3183@gmail.com>
2023-11-07 22:27:11 +0700

    Add project files.

---
+++ FundamentalProject/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FundamentalProject.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+        public ActionResult ItemPage()
+        {
+            return View();
+        }
+        public ActionResult FlightPage()
+        {
+            return View();
+        }
+        public ActionResult HotelPage()
+        {
+
+            return View();
+        }
+    }
+}
